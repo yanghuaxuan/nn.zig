7226a28dd439988c7b488fa1838a70ab3c017e0f
hyang <hyang@hyang.xyz>
2023-09-23 01:50:24 -0700

    I should add tests

--- nn.zig
+++ nn.zig
@@ -8,7 +8,6 @@ const Mat = struct {
   cols: usize,
   m: [][]f32,
 
-
   /// Returns a new dynamically allocated matrix
   pub fn New(rows: usize, cols: usize, allocator: *const Allocator) !Mat {
     const m = try allocator.alloc([]f32, rows);
@@ -25,25 +24,51 @@ const Mat = struct {
         .m = m
     };
   }
-  /// Dot products this matrix by matrix B, storing its result in dest
+  /// Dot products this matrix by matrix B, returning its result as another matrix
   ///
   /// Returns an error if the number of rows in matrix B != number of cols in this matrix
-  pub fn Dot(self: *const Mat, dest: *Mat, B: *Mat) !void {
+  pub fn Dot(self: *const Mat, B: *Mat, alo: *const Allocator) !Mat {
     if (self.cols != B.rows) {
         return error.InvalidMatrixLength;
     }
+    const row = self.rows;
+    const col = self.cols;
+
+    const m = Mat.New(row, col, alo);
+
     for (self.rows) |r| {
         for(B.cols) |c| {
-            dest[r][c] = self.m[r][c] * B.m[r][c];
+            m.m[r][c] = self.m[r][c] * B.m[r][c];
         }
     }
+
+    return m;
   }
 
-  pub fn At(self: *const Mat, i: usize, j: usize) ?f32 {
-    if (i > self.rows or j > self.cols) {
+  pub fn At(self: *const Mat, row: usize, col: usize) ?f32 {
+    if (row > self.rows or col > self.cols) {
         return null;
     }
-    return self.m[i][j];
+    return self.m[row][col];
+  }
+
+  /// Add this matrix by B, returning its result as another matrix
+  ///
+  /// Returns an error if dimensions of matrix B are not the same
+  pub fn Add(self: *const Mat, B: *const Mat, alo: *const Allocator) !Mat {
+    if (self.rows != B.rows or self.cols != self.rows) {
+        return error.InvalidMatrixLength;
+    }
+
+    const m = try Mat.New(self.rows, self.cols, alo);
+
+    for (0..self.rows) |i| {
+        for (0..self.cols) |j| {
+            m.m[i][j] = self.m[i][j] + B.m[i][j];
+        }
+    }
+    
+    return m;
   }
 
   /// Print visual representation of matrix
@@ -89,7 +114,13 @@ pub fn main() !void {
     stdprint("\n", .{});
     matrix.Randomize(1, 4);
     matrix.Print();
-    // const twobytwo_1 = try Mat.New(2, 2, &alo.allocator());
-    // const twobytwo_2 = try Mat.New(2, 2, &alo.allocator());
-    // const res = try Mat.New(2, 2, &alo.allocator());
+
+    stdprint("\n", .{});
+
+    const twobytwo_1 = try Mat.New(2, 2, &alo.allocator());
+    const twobytwo_2 = try Mat.New(2, 2, &alo.allocator());
+    twobytwo_1.Randomize(1, 2);
+    twobytwo_2.Randomize(1, 2);
+    const res = try twobytwo_1.Add(&twobytwo_2, &alo.allocator());
+    res.Print();
 }
